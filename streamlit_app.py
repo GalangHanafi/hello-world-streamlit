7126e5acd6221486e899e8943bfacc685bed23c1
Lang <93107429+GalangHanafi@users.noreply.github.com>
2024-10-01 03:35:37 +0000

    hello

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Hello Word ")
+
+name = st.text_input("nama")
+
+if (name):
+    st.warning("please, input your name!!!")
+else:
+    st.write("hallo, nama saya : ", name)
+
